7bdd8e22c076a48da84787cca994e635940f6491
sifat26 <sifatict26@gmail.com>
2025-09-08 02:21:23 +0600

    link change

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,9 +3,12 @@ import { io } from "socket.io-client";
 
 // This function connects to the backend and joins the correct restaurant room.
 export const connectOrdersSocket = (restaurantId) => {
+  // const url =
+  //   import.meta.env.VITE_API_BASE ||
+  //   "https://restaurant-project-backend-steel.vercel.app/";
   const url =
     import.meta.env.VITE_API_BASE ||
-    "https://restaurant-project-backend-steel.vercel.app/";
+    "https://urban-gril-backend.vercel.app/";
   const socket = io(url, {
     transports: ["websocket"],
     query: { restaurantId },
