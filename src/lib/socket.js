77887876fa388ecf29bd3e6124b2b285adcae264
Hosnain Rafi <rhosnain@gmail.com>
2025-09-08 13:55:57 +0600

    fix backend

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -4,8 +4,7 @@ import { io } from "socket.io-client";
 // This function connects to the backend and joins the correct restaurant room.
 export const connectOrdersSocket = (restaurantId) => {
   const url =
-    import.meta.env.VITE_API_BASE ||
-    "https://restaurant-project-backend-steel.vercel.app/";
+    import.meta.env.VITE_API_BASE || "https://urban-gril-backend.vercel.app/";
   const socket = io(url, {
     transports: ["websocket"],
     query: { restaurantId },
